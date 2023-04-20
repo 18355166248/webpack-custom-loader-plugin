eabd99ed62a655434af6fc71cd8192b45327f1dc
SMegalo <lang.jiang@ximalaya.com>
2023-04-20 18:01:53 +0800

    feat: 添加单元测试

---
+++ loaders/babel-loader/test/demo.js
@@ -0,0 +1,7 @@
+function filter() {
+  return add(1, 3);
+}
+
+const b = filter();
+
+[1, 2, 3].map((n) => n + 1);
---
+++ loaders/babel-loader/test/loader.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment node
+ */
+import compiler from "./compiler.js";
+import path from "path";
+
+test("ES2015+语法转成适用于当前浏览器的代码", async () => {
+  const stats = await compiler("demo.js", {
+    presets: ["@babel/preset-env"],
+  });
+  const output = stats.toJson({ source: true }).modules[0].source;
+
+  expect(output.replace(/\r|\n|\s/g, "")).toBe(
+    '"usestrict";functionfilter(){returnadd(1,3);}varb=filter();[1,2,3].map(function(n){returnn+1;});'
+  );
+});
